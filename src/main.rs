6cb2b7a1b94a1b2cb185e12a00f4aca4b95e96a3
enjoy-life0000 <enjoylife.follower@gmail.com>
2024-12-19 15:10:54 +0000

    creating token(testing)

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,9 @@ async fn main() -> Result<()> {
         token_program,
     );
 
+    println!("mint_account: {}", mint_account.pubkey());
+    println!("owner_account: {}", owner.pubkey());
+    println!("owing program: {}", token_program);
     let token_mint_a_ix = instruction::initialize_mint(
         token_program,
         &mint_account.pubkey(),
@@ -43,6 +46,76 @@ async fn main() -> Result<()> {
         Some(&payer.pubkey()),
         &[&payer, &mint_account],
         recent_blockhash,
+    );
+
+    banks_client
+        .process_transaction(token_mint_a_tx)
+        .await
+        .unwrap();
+
+    // Create account that can hold the newly minted tokens
+    let account_rent = rent.minimum_balance(Account::LEN);
+    let token_account = Keypair::new();
+    let new_token_account_ix = system_instruction::create_account(
+        &payer.pubkey(),
+        &token_account.pubkey(),
+        account_rent,
+        Account::LEN as u64,
+        token_program,
+    );
+
+    let my_account = Keypair::new();
+    let initialize_account_ix = instruction::initialize_account(
+        token_program,
+        &token_account.pubkey(),
+        &mint_account.pubkey(),
+        &my_account.pubkey(),
+    )
+    .unwrap();
+
+    let create_new_token_account_tx = Transaction::new_signed_with_payer(
+        &[new_token_account_ix, initialize_account_ix],
+        Some(&payer.pubkey()),
+        &[&payer, &token_account],
+        recent_blockhash,
+    );
+    banks_client
+        .process_transaction(create_new_token_account_tx)
+        .await
+        .unwrap();
+
+    // Mint tokens into newly created account
+    let mint_amount: u64 = 10;
+    let mint_to_ix = instruction::mint_to(
+        &token_program,
+        &mint_account.pubkey(),
+        &token_account.pubkey(),
+        &owner.pubkey(),
+        &[],
+        mint_amount.clone(),
+    )
+    .unwrap();
+
+    let mint_to_tx = Transaction::new_signed_with_payer(
+        &[mint_to_ix],
+        Some(&payer.pubkey()),
+        &[&payer, &owner],
+        recent_blockhash,
+    );
+    banks_client.process_transaction(mint_to_tx).await.unwrap();
+
+    // Inspect account
+    let token_account_info = banks_client
+        .get_account(token_account.pubkey().clone())
+        .await
+        .unwrap()
+        .expect("could not fetch account information");
+    let account_data = Account::unpack(&token_account_info.data).unwrap();
+    println!("account data: {:?}", account_data);
+    assert_eq!(
+        account_data.amount,
+        mint_amount.clone(),
+        "not correct amount"
     );
     Ok(())
 }
